cc2120e1c8831ad4be8405d7a2a002aea6e4c0a1
Rohan Batra <rohanbatra24@gmail.com>
2020-07-05 17:10:43 -0400

    add filter functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,32 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+
 import Like from './common/like';
 import Pagination from './common/pagination';
+import Filter from './common/filter';
 
 import { paginate } from '../utils/paginate';
 
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 
 export default function Movies() {
-	const [ movies, setMovies ] = useState({ movieList: getMovies() });
+	const [ movies, setMovies ] = useState([]);
 	const [ pageSize, setPageSize ] = useState({ pageSize: 4 });
 	const [ currPage, setCurrPage ] = useState(1);
+	const [ genres, setGenres ] = useState([]);
+	const [ selectedGenre, setSelectedGenre ] = useState('');
+
+	useEffect(() => {
+		const genres = [ { name: 'All Genres' }, ...getGenres() ];
+
+		setGenres(genres);
+		setMovies({ movieList: getMovies() });
+	}, []);
+
+	const handleFilter = (genre) => {
+		setCurrPage(1);
+		setSelectedGenre(genre);
+	};
 
 	const handlePageChange = (page) => {
 		setCurrPage(page);
@@ -35,57 +52,69 @@ export default function Movies() {
 		setMovies({ movieList: moviesCopy });
 	};
 
-	if (movies.movieList.length === 0) {
+	if (!movies.movieList || movies.movieList.length === 0) {
 		return <p>There are no movies in the database.</p>;
 	}
 
+	const filtered =
+		selectedGenre && selectedGenre._id
+			? movies.movieList.filter((movie) => movie.genre.name === selectedGenre.name)
+			: movies.movieList;
+
+	const moviesPaginated = paginate(filtered, pageSize.pageSize, currPage);
+
 	return (
-		<Fragment>
-			<h1>Showing {movies.length} movies in the database</h1>
+		<div className="row">
+			<div className="col-3">
+				<Filter onFilterClick={handleFilter} items={genres} selectedItem={selectedGenre} />
+			</div>
+			<div className="col">
+				<h1>Showing {filtered.length} movies in the database</h1>
 
-			<table className="table">
-				<thead>
-					<tr>
-						<th scope="col">Title</th>
-						<th scope="col">Genre</th>
-						<th scope="col">Stock</th>
-						<th scope="col">Rate</th>
-						<th scope="col" />
-						<th scope="col" />
-					</tr>
-				</thead>
-				<tbody>
-					{paginate(movies.movieList, pageSize.pageSize, currPage).map((movie) => {
-						return (
-							<tr key={movie._id}>
-								<th scope="row">{movie.title}</th>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>@{movie.dailyRentalRate}</td>
-								<td>
-									<Like liked={movie.liked} onLike={onLike} movie={movie} />
-								</td>
+				<table className="table">
+					<thead>
+						<tr>
+							<th scope="col">Title</th>
+							<th scope="col">Genre</th>
+							<th scope="col">Stock</th>
+							<th scope="col">Rate</th>
+							<th scope="col" />
+							<th scope="col" />
+						</tr>
+					</thead>
+					<tbody>
+						{moviesPaginated.map((movie) => {
+							return (
+								<tr key={movie._id}>
+									<th scope="row">{movie.title}</th>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>@{movie.dailyRentalRate}</td>
+									<td>
+										<Like liked={movie.liked} onLike={onLike} movie={movie} />
+									</td>
 
-								<td>
-									<button
-										onClick={() => onDelete(movie._id)}
-										key={movie._id}
-										className="btn btn-danger btn-sm"
-									>
-										Delete
-									</button>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-			<Pagination
-				itemsCount={movies.movieList.length}
-				pageSize={pageSize.pageSize}
-				onPageChange={handlePageChange}
-				currPage={currPage}
-			/>
-		</Fragment>
+									<td>
+										<button
+											onClick={() => onDelete(movie._id)}
+											key={movie._id}
+											className="btn btn-danger btn-sm"
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+				<Pagination
+					itemsCount={filtered.length}
+					pageSize={pageSize.pageSize}
+					onPageChange={handlePageChange}
+					currPage={currPage}
+				/>
+			</div>
+		</div>
 	);
 }
