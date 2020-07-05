65c5e83a8d7b075955457cfd9a335ec4201d2b72
Rohan Batra <rohanbatra24@gmail.com>
2020-07-05 17:10:53 -0400

    create filter component

---
+++ src/components/common/filter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Filter({ onFilterClick, items, textProperty, valueProperty, selectedItem }) {
+	const itemList = items.map((item) => {
+		return (
+			<li
+				type="button"
+				className={item.name === selectedItem ? 'list-group-item active' : 'list-group-item'}
+				key={item[valueProperty]}
+				onClick={() => onFilterClick(item)}
+			>
+				{item[textProperty]}
+			</li>
+		);
+	});
+
+	return <ul className="list-group">{itemList}</ul>;
+}
+
+Filter.defaultProps = {
+	textProperty  : 'name',
+	valueProperty : '_id'
+};
