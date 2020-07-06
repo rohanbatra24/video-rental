033286e86ffdeced3c2f182f8d3a673ce7b94771
Rohan Batra <rohanbatra24@gmail.com>
2020-07-06 13:54:40 -0400

    add table header logic

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-export default function TableHeader() {
+export default function TableHeader({ handleSort, columns, sortColumn }) {
+	const raiseSort = (column) => {
+		if (column === sortColumn.column && sortColumn.order === 'asc') {
+			handleSort({ column, order: 'desc' });
+		}
+		else {
+			handleSort({ column, order: 'asc' });
+		}
+	};
+
 	return (
 		<thead>
 			<tr>
-				{props.columns.map((column) => {
-					return <th>{column.label}</th>;
+				{columns.map((column) => {
+					return (
+						<th key={column.path || column.key} onClick={() => raiseSort(column.path)}>
+							{column.label}
+						</th>
+					);
 				})}
 			</tr>
 		</thead>
