62ac01203b23688ce41b078be5e6e7c9e1811f11
Rohan Batra <rohanbatra24@gmail.com>
2020-07-06 13:54:11 -0400

    move table header logic into seperate new component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,61 +1,22 @@
 import React from 'react';
-import Like from './common/like';
 
-export default function MoviesTable({ movies, sortColumn, onLike, onDelete, onSort, handleSort }) {
-	const raiseSort = (column) => {
-		if (column === sortColumn.column && sortColumn.order === 'asc') {
-			handleSort({ column, order: 'desc' });
-		}
-		else {
-			handleSort({ column, order: 'asc' });
-		}
-	};
+import TableHeader from './common/tableHeader';
+import TableBody from './common/tableBody';
+
+export default function MoviesTable({ movies, sortColumn, onLike, onDelete, handleSort }) {
+	const columns = [
+		{ path: 'title', label: 'Title' },
+		{ path: 'genre.name', label: 'Genre' },
+		{ path: 'numberInStock', label: 'Stock' },
+		{ path: 'dailyRentalRate', label: 'Rate' },
+		{ key: 'like' },
+		{ key: 'delete' }
+	];
 
 	return (
 		<table className="table">
-			<thead>
-				<tr>
-					<th onClick={() => raiseSort('title')} scope="col">
-						Title
-					</th>
-					<th onClick={() => raiseSort('genre.name')} scope="col">
-						Genre
-					</th>
-					<th onClick={() => raiseSort('numberInStock')} scope="col">
-						Stock
-					</th>
-					<th onClick={() => raiseSort('dailyRentalRate')} scope="col">
-						Rate
-					</th>
-					<th scope="col" />
-					<th scope="col" />
-				</tr>
-			</thead>
-			<tbody>
-				{movies.map((movie) => {
-					return (
-						<tr key={movie._id}>
-							<th scope="row">{movie.title}</th>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-							<td>
-								<Like liked={movie.liked} onLike={onLike} movie={movie} />
-							</td>
-
-							<td>
-								<button
-									onClick={() => onDelete(movie._id)}
-									key={movie._id}
-									className="btn btn-danger btn-sm"
-								>
-									Delete
-								</button>
-							</td>
-						</tr>
-					);
-				})}
-			</tbody>
+			<TableHeader handleSort={handleSort} columns={columns} sortColumn={sortColumn} />
+			<TableBody movies={movies} onLike={onLike} onDelete={onDelete} />
 		</table>
 	);
 }
