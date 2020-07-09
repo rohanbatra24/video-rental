757bd094c43f4425e647abc445fe89bfa9481edb
Rohan Batra <rohanbatra24@gmail.com>
2020-07-09 10:37:49 -0400

    create reusable input form

---
+++ src/components/common/input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Input(props) {
+	console.log('props', props);
+	return (
+		<div className="form-group">
+			<label htmlFor={props.name}>{props.label}</label>
+			<input
+				value={props.value}
+				name={props.name}
+				id={props.name}
+				type="text"
+				className="form-control"
+				onChange={props.onChange}
+			/>
+			{props.errors && <div className="alert alert-danger">{props.errors}</div>}
+		</div>
+	);
+}
