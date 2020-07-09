f8981b1a022ef623171e9494287703c6b50c8635
Rohan Batra <rohanbatra24@gmail.com>
2020-07-08 21:25:52 -0400

    add routes to app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Movies from './components/movies';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import NavBar from './components/common/navBar';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import MovieForm from './components/movieForm';
+
+import NotFound from './components/common/notFound';
 
 function App() {
 	return (
-		<main className="container">
-			<Movies />
-		</main>
+		<Fragment>
+			<NavBar />
+			<main className="container">
+				<Switch>
+					<Route path="/customers" component={Customers} />
+					<Route path="/rentals" component={Rentals} />
+					<Route path="/movies/:id" component={MovieForm} />
+					<Route path="/movies" exact component={Movies} />
+					<Route path="/not-found" component={NotFound} />
+					<Redirect from="/" exact to="/movies" />
+					<Redirect to="/not-found" />
+				</Switch>
+			</main>
+		</Fragment>
 	);
 }
 
