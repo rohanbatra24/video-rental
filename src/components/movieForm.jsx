d017079007b57128b3124e2f22f61c326329979e
Rohan Batra <rohanbatra24@gmail.com>
2020-07-08 21:26:30 -0400

    create movie form component

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MovieForm(props) {
+	return (
+		<div>
+			<h1>Movie Form</h1>
+			<h2>{props.match.params.id}</h2>
+
+			<button className="btn btn-primary" onClick={() => props.history.push('/movies')}>
+				Save
+			</button>
+		</div>
+	);
+}
