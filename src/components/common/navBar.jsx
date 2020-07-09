d54e67e489058bfeb9b1a45896ff5e6aa372358a
Rohan Batra <rohanbatra24@gmail.com>
2020-07-08 21:27:03 -0400

    create navbar component

---
+++ src/components/common/navBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+export default function NavBar() {
+	return (
+		<nav class="navbar navbar-expand-lg navbar-light bg-light m-4">
+			<Link className="navbar-brand" to="/">
+				Vidly
+			</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<NavLink className="nav-item nav-link" to="/movies">
+						Movies
+					</NavLink>
+					<NavLink className="nav-item nav-link" to="/customers">
+						Customers
+					</NavLink>
+					<NavLink className="nav-item nav-link" to="/rentals">
+						Rentals
+					</NavLink>
+				</div>
+			</div>
+		</nav>
+	);
+}
