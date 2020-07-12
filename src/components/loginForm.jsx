5f6cfd8f158c0b4c0a3374d1057d8124ca9ce83a
Rohan Batra <rohanbatra24@gmail.com>
2020-07-11 21:23:35 -0400

    add schema for joi, helper functions for validate property, handle change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Input from './common/input';
+import Joi from 'joi-browser';
 
 export default function LoginForm() {
 	const [ user, setUser ] = useState({ username: '', password: '' });
 	const [ errors, setErrors ] = useState({});
 
+	const schema = {
+		username : Joi.string().required().label('Username'),
+		password : Joi.string().required().label('Password')
+	};
+
 	const validate = () => {
+		const result = Joi.validate(user, schema, { abortEarly: false });
 		const errors = {};
 
-		if (user.username.trim() === '') {
-			errors.username = 'Username is required.';
+		if (!result.error) return null;
+
+		for (let error of result.error.details) {
+			errors[error.path[0]] = error.message;
 		}
 
-		if (user.password.trim() === '') {
-			errors.password = 'Password is required.';
-		}
-
-		return Object.values(errors).length === 0 ? null : errors;
+		return errors;
 	};
 
 	const handleSubmit = (event) => {
@@ -24,26 +29,56 @@ export default function LoginForm() {
 
 		const errors = validate();
 
-		console.log('errors', errors);
-
 		setErrors(errors || {});
 
 		if (errors) return;
 
 		//call server
-		// console.log(user);
+	};
+
+	const validateProperty = (name, value) => {
+		const obj = { [name]: value };
+
+		const subSchema = {
+			[name] : schema[name]
+		};
+
+		const error = Joi.validate(obj, subSchema);
+
+		if (!error.error) return null;
+		return error.error.details[0].message;
+	};
+
+	const handleChange = (e) => {
+		const errorsCopy = { ...errors };
+
+		const errorMessage = validateProperty(e.currentTarget.name, e.currentTarget.value);
+
+		if (errorMessage) {
+			errorsCopy[e.currentTarget.name] = errorMessage;
+		}
+		else delete errorsCopy[e.currentTarget.name];
+
+		const userCopy = { ...user };
+
+		userCopy[e.currentTarget.name] = e.currentTarget.value;
+
+		setErrors(errorsCopy || {});
+		setUser(userCopy);
+
+		//call server
 	};
 
 	return (
 		<div>
 			<h1>Login</h1>
 
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={(event) => handleSubmit(event)}>
 				<Input
 					value={user.username}
 					name="username"
 					label="Username"
-					onChange={(event) => setUser({ ...user, username: event.target.value })}
+					onChange={handleChange}
 					errors={errors.username}
 				/>
 
@@ -51,10 +86,12 @@ export default function LoginForm() {
 					value={user.password}
 					name="password"
 					label="Password"
-					onChange={(event) => setUser({ ...user, password: event.target.value })}
+					onChange={handleChange}
 					errors={errors.password}
 				/>
-				<button className="btn btn-primary">Login</button>
+				<button disabled={validate()} className="btn btn-primary">
+					Login
+				</button>
 			</form>
 		</div>
 	);
